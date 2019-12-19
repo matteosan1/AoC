5442e1ac24328df9ea82b60262100453f0536296
Matteo Sani <matteosan1@gmail.com>
2019-12-19 17:44:48 +0100

    Add files via upload

--- 2019/19b.py
+++ 2019/19b.py
@@ -174,52 +174,18 @@ class Computer:
 manual_stopping = False
 
 q_in = Queue()
-q_out = Queue()
-beam_map = {}
 
-points = 0
-for y in range(50):
-    for x in range(50):
-        c = Computer("A", code, q_in)#, q_out)
-        c.inputs.append(1000)
-        c.inputs.append(x)
-        c.inputs.append(y)
-        #process = Thread(target=c.run, args=[manual_stopping])
-        c.run(manual_stopping) #.start()
-        if c.outputs[0] == 1:
-            beam_map[(x, y)] = c.outputs[0]
-            points += 1
+def check(x, y):
+    c = Computer("A", code, q_in)
+    c.inputs.append(0)
+    c.inputs.append(x)
+    c.inputs.append(y)
+    c.run(manual_stopping)
+    return c.outputs[0]
 
-ymin = min([x[1] for x in beam_map.keys() if x[0] == 49])
-ymax = max([x[1] for x in beam_map.keys() if x[0] == 49])
-print (ymin, ymax)
+x = box = 100
+for y in range(box, 99999):
+    while check(x, y) == 0: x += 1
+    if check(x+box-1, y-box+1) == 1: break
 
-xmin = min([x[0] for x in beam_map.keys() if x[0] != 0])
-print (xmin)
-print (beam_map)
-def line(x1, x2):
-    m = (x2[1] - x1[1])/(x2[0] - x1[0])
-    q = x1[1] - m*x1[0]
-    return m,q
-
-x1 = (4, 3)
-x2 = (49, 36)
-x3 = (49, 44)
-m1, q1 = line(x1, x2)
-m2, q2 = line(x1, x3)
-
-for x in range(1500):
-    y1 = m1*x + q1
-
-    print (x, y1)
-
-#for y in range(50):
-#    for x in range(50):
-#        if beam_map[(x, y)] == 1:
-#            print ("#", end='')
-#        else:
-#            print (".", end='')
-#    print ("")
-
-#print (points)
-
+print(f"Closest point to fit santa's ship {(x, y-99)}: {x * 10000 + y-99}")
---
+++ 2019/advent.py
@@ -0,0 +1,142 @@
+import os
+import sys
+import re
+from importlib import find_loader
+from datetime import datetime, timedelta
+
+def log(s, *a):
+	sys.stderr.write(s.format(*a))
+	sys.stderr.flush()
+
+def check_or_die(resp):
+	pls_identify = 'please identify yourself' in resp.text.lower()
+
+	if resp.status_code != 200:
+		log('\n[advent] ERROR: response {}, url: {}\n', resp.status_code, resp.url)
+		log('[advent] Did you log in and update your session cookie?\n')
+		sys.exit(1)
+	elif pls_identify:
+		log('\n[advent] ERROR: Server returned 200, but is asking for identification.\n')
+		log('[advent] Did you log in and update your session cookie?\n')
+		sys.exit(1)
+
+def check_setup_once():
+	if YEAR == -1 and DAY == -1:
+		now = datetime.utcnow() + timedelta(hours=-5)
+		y, m, d = now.year, now.month, now.day
+
+		if m != 12 or (m == 12 and d > 25):
+			log('[advent] ERROR: year and day not set, and no event currently running!\n')
+			sys.exit(1)
+
+		log('[advent] Year and day not set, assuming today: Dec {}, {}.\n', d, y)
+		setup(y, d)
+
+def setup(year, day, dry_run=False):
+	global YEAR
+	global DAY
+	global DRY_RUN
+	global SESSION
+
+	assert year >= 2015
+	assert 1 <= day <= 25
+
+	YEAR    = year
+	DAY     = day
+	DRY_RUN = dry_run
+
+	if REQUESTS and os.path.isfile('secret_session_cookie'):
+		with open('secret_session_cookie') as f:
+			SESSION = f.read().rstrip()
+			s.cookies.set('session', SESSION)
+
+def get_input(fname=None, mode='r'):
+	check_setup_once()
+
+	if not os.path.isdir(CACHE_DIR):
+		try:
+			os.mkdir(CACHE_DIR)
+			log("[advent] Created cache directory '{}' since it did not exist.\n", CACHE_DIR)
+		except Exception as e:
+			log("[advent] ERROR: could not create cache directory '{}'.\n", CACHE_DIR)
+			log('[advent] {}\n', str(e))
+			sys.exit(1)
+
+	log('[advent] Getting input for year {} day {}... ', YEAR, DAY)
+
+	if fname is None:
+		fname = os.path.join(CACHE_DIR, '{}_{:02d}.txt'.format(YEAR, DAY))
+
+	if not os.path.isfile(fname):
+		if not REQUESTS:
+			log('err!\n')
+			log('[advent] ERROR: cannot download input, no requests module installed!\n')
+			sys.exit(1)
+		elif not SESSION:
+			log('err!\n')
+			log('[advent] ERROR: cannot download input file without session cookie!\n')
+			sys.exit(1)
+
+		log('downloading... ')
+
+		r = s.get(URL.format(YEAR, DAY, 'input'))
+		check_or_die(r)
+
+		with open(fname, 'wb') as f:
+			f.write(r.content)
+
+		log('done.\n')
+
+	else:
+		log('done (from disk).\n')
+
+	return open(fname, mode)
+
+def submit_answer(part, answer):
+	check_setup_once()
+
+	if DRY_RUN:
+		print('Part {}: {}'.format(part, answer))
+	elif not REQUESTS:
+		log('[advent] Cannot upload answer, no requests module installed!\n')
+		print('Part {}: {}'.format(part, answer))
+	else:
+		log('[advent] Submitting day {} part {} answer: {}\n', DAY, part, answer)
+
+		r = s.post(URL.format(YEAR, DAY, 'answer'), data={'level': part, 'answer': answer})
+		check_or_die(r)
+
+		t = r.text.lower()
+
+		if 'did you already complete it' in t:
+			log('[advent] Already completed!\n')
+			return True
+
+		if "that's the right answer" in t:
+			log('[advent] Right answer!\n')
+			return True
+
+		if 'you have to wait' in t:
+			matches = re.compile(r'you have ([\w ]+) left to wait').findall(t)
+
+			if matches:
+				log('[advent] Submitting too fast, {} left to wait.\n', matches[0])
+			else:
+				log('[advent] Submitting too fast, slow down!\n')
+
+			return False
+
+		log('[advent] Wrong answer :(\n')
+		return False
+
+URL       = 'https://adventofcode.com/{:d}/day/{:d}/{:s}'
+SESSION   = ''
+CACHE_DIR = './inputs'
+YEAR      = -1
+DAY       = -1
+DRY_RUN   = False
+REQUESTS  = find_loader('requests')
+
+if REQUESTS:
+	import requests
+	s = requests.Session()
