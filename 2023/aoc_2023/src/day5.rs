059bf57e7ba03f54a421f9dfb94c9be7e5636b8a
Matteo Sani <matteosan1@gmail.com>
2023-12-04 22:24:05 +0100

    [UPD] Day 5

---
+++ 2023/aoc_2023/src/day5.rs
@@ -0,0 +1,15 @@
+pub mod day5 {
+    extern crate aoc;
+    use aoc::utils;
+
+    pub fn part1(input: &Vec<String>) {
+        
+        println!("{} {}", utils::santa(5, 1), );
+    }
+
+
+    pub fn part2(input: &Vec<String>) {
+        
+        println!("{} {}", utils::christmas_tree(5, 2), );
+    }
+}
--- 2023/aoc_2023/src/main.rs
+++ 2023/aoc_2023/src/main.rs
@@ -3,13 +3,13 @@ use std::time::Instant;
 extern crate aoc;
 use aoc::utils;
 
-mod day4;
+mod day5;
 
 fn main() {
     let now = Instant::now();
-    let input = utils::read_input("../input_4.txt");
-    day4::day4::part1(&input);
-    day4::day4::part2(&input);
+    let input = utils::read_input("../input_5.txt");
+    day5::day5::part1(&input);
+    day5::day5::part2(&input);
 
     let elapsed = now.elapsed();
     println!("Elapsed: {:.2?}", elapsed); 
