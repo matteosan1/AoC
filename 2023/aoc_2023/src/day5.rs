4b5aca17a026524393abe7bc46124295be0a522f
Matteo Sani <matteosan1@gmail.com>
2023-12-05 21:37:29 +0100

    [UPD] day 5

--- 2023/aoc_2023/src/day5.rs
+++ 2023/aoc_2023/src/day5.rs
@@ -1,15 +1,78 @@
+// implementare parte 2 location to seed...
 pub mod day5 {
     extern crate aoc;
     use aoc::utils;
 
-    pub fn part1(input: &Vec<String>) {
+    #[derive(Copy, Clone)]
+    struct Range {
+        dest: i64,
+        source: i64,
+        length: i64,
+    }
+    
+    fn preprocess(input: &Vec<String>) -> (Vec::<i64>, Vec::<Vec::<Range>>) {
+        let mut data = Vec::<Vec::<Range>>::new();
+        let mut seeds = Vec::<i64>::new();
+        let mut ranges = Vec::<Range>::new();
         
-        println!("{} {}", utils::santa(5, 1), );
+        for l in input {
+            if l.contains("seeds:") {
+                let x = match l.split_once("seeds:") {
+                    Some(x) => x.1,
+                    _ => "",
+                };
+                seeds = x.split_whitespace().map(|x| x.parse::<i64>().unwrap()).collect();
+            } else if l.ends_with("map:") {
+                if ranges.len() > 0 {
+                    data.push(ranges.clone());
+                    ranges.clear();
+                }
+            } else if l == "" {
+                continue;
+            } else {
+                let x: Vec<i64> = l.split_whitespace().map(|x| x.parse::<i64>().unwrap()).collect();
+                let range = Range { dest: x[0], source: x[1], length: x[2] };
+                ranges.push(range);
+            }
+        }
+        data.push(ranges);
+        //    for k, v in data.items():
+        //        if k == 'seed':
+        //            continue
+        //        data[k].sort(key=itemgetter(1))
+        (seeds, data)
+    }
+
+    fn mapping(seed: &i64, map: &Range) -> Result<i64, &'static str> {
+        if *seed >= map.source && *seed <= map.source+map.length {
+            return Ok((seed - map.source) + map.dest);
+        }
+
+        Err("not in range")
+    }
+    
+    pub fn part1(input: &Vec<String>) {
+        let (mut seeds, maps) = preprocess(input);
+        for map in maps {
+            for i in 0..seeds.len() {
+                for m in &map {
+                    match mapping(&seeds[i], &m) {
+                        Ok(x) => {
+                            seeds[i] = x;
+                            break;
+                        },
+                        Err(_x) => continue
+                    };
+                }
+            }
+        }
+        let min_location = seeds.iter().min().unwrap();
+        println!("{} {}", utils::santa(5, 1), min_location);
     }
 
 
     pub fn part2(input: &Vec<String>) {
         
-        println!("{} {}", utils::christmas_tree(5, 2), );
+        //println!("{} {}", utils::christmas_tree(5, 2), );
     }
 }
---
+++ 2023/aoc_2023/src/day6.rs
@@ -0,0 +1,15 @@
+pub mod day6 {
+    extern crate aoc;
+    use aoc::utils;
+
+    pub fn part1(input: &Vec<String>) {
+        
+        println!("{} {}", utils::santa(6, 1), );
+    }
+
+
+    pub fn part2(input: &Vec<String>) {
+        
+        println!("{} {}", utils::christmas_tree(6, 2), );
+    }
+}
