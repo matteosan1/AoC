cff15f53ea971a76ab8f95f9d32f0d91ccba6cf1
Matteo Sani <matteo.sani@mpscapitalservices.it>
2023-12-12 15:23:27 +0100

    [UPD] day 12 python

--- 2023/12.py
+++ 2023/12.py
@@ -1,26 +1,63 @@
 import time
+from functools import cache
+
 from utils import readInput
 
 def loadInput():
     lines = readInput("prova.txt")
     #lines = readInput("input_12.txt")
-    return lines
+              
+    inputs = []      
+    for l in lines:
+        parts = l.split(" ")
+        inputs.append([parts[0], 
+                      tuple(map(int, parts[1].split(",")))])
+    return inputs
+    
+@cache
+def numlegal(s, c):
+    s = s.lstrip('.') # ignore leading dots
 
+    # extreme cases
+    # ['', ()] is legal
+    if s == '':
+        return int(c == ()) 
+
+    # [s, ()] is legal so long as s has no '#' (we can convert '?' to '.')
+    if c == ():
+        return int(s.find('#') == -1) 
+
+    # s starts with '#' so remove the first spring
+    if s[0] == '#':
+        if len(s) < c[0] or '.' in s[:c[0]]:
+            return 0 # impossible - not enough space for the spring
+        elif len(s) == c[0]:
+            return int(len(c) == 1) #single spring, right size
+        elif s[c[0]] == '#':
+            return 0 # springs must be separated by '.' (or '?') 
+        else:
+            return numlegal(s[c[0]+1:],c[1:]) # one less spring
+
+    # numlegal springs if we convert the first '?' to '#' + '.'
+    return numlegal('#'+s[1:],c) + numlegal(s[1:],c)
+    
 def part1(inputs):
-    pass
-    #return res1
+    res = [numlegal(s,c) for [s,c] in inputs]
+    print (res)
+    return sum(res)
 
 def part2(inputs):
-    pass
-    #return res2
+    ss2 = [[(s[0]+'?')*4 + s[0],s[1]*5] for s in inputs]
+    res = [numlegal(s,c) for [s,c] in ss2]
+    return sum(res)
 
 if __name__ == '__main__':
-    print("⛄⛄⛄⛄⛄⛄⛄⛄⛄⛄⛄⛄⛄")
-    print("⛄        Day 12         ⛄")
-    print("⛄⛄⛄⛄⛄⛄⛄⛄⛄⛄⛄⛄⛄")
-    
-    inputs = loadInput()
+    #print("⛄⛄⛄⛄⛄⛄⛄⛄⛄⛄⛄⛄⛄")
+    #print("⛄        Day 12         ⛄")
+    #print("⛄⛄⛄⛄⛄⛄⛄⛄⛄⛄⛄⛄⛄")
 
+    inputs = loadInput()
+    
     t0 = time.time()
     res1 = part1(inputs)
     t1 = time.time()-t0
