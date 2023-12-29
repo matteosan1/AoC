d12cd8dbe4e1d66e70a3000b45fc98f2ceb128c3
Matteo Sani <matteo.sani@mpscapitalservices.it>
2023-12-29 11:13:09 +0100

    [UPD] Change 24/2015 a liitle bit slower but clearer

--- 2015/24.py
+++ 2015/24.py
@@ -9,24 +9,40 @@ def loadInput():
     lines = readInput("instructions24a.txt")
     return list(map(int, lines))
 
+def get_groups(lst, parts, tot, ng, g1=None):
+    if g1 is None:
+        y = 1
+    else:
+        y = len(g1[0])+1
+    while y < len(lst)+1:
+        for z in combinations(lst, y):
+            if sum(z) == tot:
+                if ng == parts:
+                    entanglement = math.prod(z)
+                    if g1 is not None:
+                        if entanglement < g1[1]:
+                            g1 = [z, entanglement, False]
+                        else:
+                            continue
+                    else:
+                        g1 = [z, entanglement, False]
+                elif ng == 1:
+                    g1[2] = True
+                get_groups(list(set(lst) - set(z)), parts, tot, ng - 1, g1)                     
+        if g1 is not None and g1[2]:
+            break
+        y += 1
+    return g1
+    
 def solve(inputs, parts):
     tot = sum(inputs)/parts
-    def hasSum(lst, sub):
-        for y in range(1, len(lst)):
-            for x in (z for z in combinations(lst, y) if sum(z) == tot):
-                if sub == 2:
-                    return True
-                elif sub < parts:
-                    return hasSum(list(set(lst) - set(x)), sub - 1)
-                elif hasSum(list(set(lst) - set(x)), sub - 1):
-                    return math.prod(x)
-    return hasSum(inputs, parts)
-
+    return get_groups(inputs, parts, tot, parts)
+    
 def part1(inputs):
-    print (f"🎄 Part 1: {solve(inputs, 3)}")
+    print (f"🎄 Part 1: {solve(inputs, 3)[1]}")
 
 def part2(inputs):
-    print (f"🎄🎅 Part 2: {solve(inputs, 4)}")
+    print (f"🎄🎅 Part 2: {solve(inputs, 4)[1]}")
     
 if __name__ == "__main__":
     title = "Day 24: It Hangs in the Balance"
